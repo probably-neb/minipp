549efb16165af0e556d16d6c3fff9369c1c2047e
Ben Kunkle <ben.kunkle@gmail.com>
2024-04-19 13:20:27 -0700

    make find ast node usages count script only count '.{Node}' to remove false positives

--- scripts/find_ast_node_usage_counts.sh
+++ scripts/find_ast_node_usage_counts.sh
@@ -4,6 +4,6 @@ files=$(cat src/ast.zig | rg '\s*\w+: struct' | cut -d: -f1 | cut -b 9-)
 
 # thank you perplex-ippity
 while IFS=$'\n' read -r line; do
-    matches=$(rg -c "$line" src/parser.zig)
+    matches=$(rg -c "\.$line" src/parser.zig)
     echo "$line: $matches"
 done <<< "$files"
