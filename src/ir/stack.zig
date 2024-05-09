27d9ba6fdc914b2701a09d1b755190139694a36b
Ben Kunkle <ben.kunkle@gmail.com>
2024-05-09 09:03:11 -0700

    get opimizationbenchmark compiling by making params pointers :)

--- src/ir/stack.zig
+++ src/ir/stack.zig
@@ -528,9 +528,9 @@ fn gen_expression(
                 .Identifier => ident: {
                     const identID = ir.internToken(ast, atom.token);
                     const ref = try fun.getNamedRef(ir, identID);
-                    if (ref.kind == .param) {
-                        break :ident ref;
-                    }
+                    // if (ref.kind == .param) {
+                    //     break :ident ref;
+                    // }
                     const inst = Inst.load(ref.type, ref);
                     const res = try fun.addNamedInst(bb, inst, ref.name, ref.type);
                     break :ident IR.Ref.fromReg(res);
@@ -1588,6 +1588,32 @@ test "stack.suite.prime" {
         "exit:",
         "  %_38 = load i1, i1* %_0",
         "  ret i1 %_38",
+        "}",
+    });
+}
+
+test "stack.neg-is-type-int" {
+    try expectResultsInIR(
+        \\fun main() void {
+        \\  int a, b;
+        \\  a = -1;
+        \\  b = a + 1;
+        \\}
+    , .{
+        "define void @main() {",
+        "entry:",
+        "  %a1 = alloca i64",
+        "  %b2 = alloca i64",
+        "1:",
+        "  %a2 = load i64, i64* %a1",
+        "  %a3 = neg i64 0, %a2",
+        "  store %i64 %a3, %i64* %a1",
+        "  %a5 = load i64, i64* %a1",
+        "  %a6 = add i64, %a5 1",
+        "  store i64 %a6, i64* %b2",
+        "  br label %exit",
+        "exit:",
+        "  ret void",
         "}",
     });
 }
