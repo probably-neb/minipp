be903f5d599e8d73b1146f50b8fc3152d0b7d45f
spenc <spenc@archSpenc>
2024-05-25 13:49:23 -0700

    Added Files for dead code and will remove file for const fold

---
+++ src/ir/deadCodeElim.zig
@@ -0,0 +1,23 @@
+pub const std = @import("std");
+const log = @import("../log.zig");
+
+const irPhi = @import("./ir_phi.zig");
+
+pub fn markDeadCode(function: *irPhi.Function) *irPhi.Function {
+    // Psuedocode:
+    // Make a map of registers to (value, isConstant)
+    // for each block in the function
+    //    for each instruction in the block
+    //        for each register in the instruction
+    //            if the map[register] is a constant:
+    //                replace the register with the constant
+    //
+    //        if the instruction is made up of constants:
+    //             remove the instruction and...
+    //             Set the map[register] to the constant
+
+    return function;
+}
+
+pub 
+
