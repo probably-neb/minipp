1dbef387080940f1bba2a31d25a90275989764be
spenc <spenc@archSpenc>
2024-05-26 16:20:24 -0700

    Added Imports and deffinitions

--- src/ir/deadCodeElim.zig
+++ src/ir/deadCodeElim.zig
@@ -1,7 +1,21 @@
 pub const std = @import("std");
+const Alloc = std.mem.Allocator;
+const ArrayList = std.ArrayList;
+
+const utils = @import("../utils.zig");
 const log = @import("../log.zig");
 
-const irPhi = @import("./ir_phi.zig");
+const IR = @import("./ir_phi.zig");
+const Function = IR.Function;
+const BasicBlock = IR.BasicBlock;
+const BBID = BasicBlock.ID;
+const Inst = IR.Inst;
+const InstID = Function.InstID;
+const Register = IR.Register;
+const Reg = Register;
+const RegID = Register.ID;
+const Ref = IR.Ref;
+const OpCode = IR.Op;
 
 pub fn markDeadCode(function: *irPhi.Function) *irPhi.Function {
     // Psuedocode:
